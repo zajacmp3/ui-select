a6dfdec42675614e5195f51612b81c048c23d284
Mateusz Zajac <m.zajac@kainos.com>
2016-10-20 06:51:14 -0700

    Corrected tests

--- src/uiSelectDirective.js
+++ src/uiSelectDirective.js
@@ -121,7 +121,7 @@ uis.directive('uiSelect',
           }
         });
 
-        $select.objectUniqueKey = $parse(attrs.objectUniqueKey);
+        $select.objectUniqueKey = attrs.objectUniqueKey;
         attrs.$observe('objectUniqueKey', function() {
           if(attrs.objectUniqueKey !== undefined) {
             $select.objectUniqueKey = attrs.objectUniqueKey;
